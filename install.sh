6d3fa8e8e96816086bae8c57ba5e302a28e5ab93
Tom-Erik Duestad <tom-erik@safeconsult.no>
2017-07-05 11:02:00 +0200

    Use ohmyzsh plugin for custom stuff
    Update zshrc to latest version

--- install.sh
+++ install.sh
@@ -1,26 +1,9 @@
 #!/usr/bin/env bash
 
-git submodule init
-git submodule update
-
 cd ~
-ln -sh dotfiles/bash_profile .bash_profile
-ln -sh dotfiles/bashrc .bashrc
-# ln -sh dotfiles/bin bin
-# ln -sh dotfiles/caprc .caprc
-# ln -sh dotfiles/gemrc .gemrc
-# ln -sh dotfiles/gitconfig .gitconfig
-# ln -sh dotfiles/githelpers .githelpers
-# ln -sh dotfiles/gitignore .gitignore
-# ln -sh dotfiles/git-prompt .git-prompt
-# ln -sh dotfiles/helpers helpers
-# ln -sh dotfiles/hyperterm.js .hyperterm.js
-# ln -sh dotfiles/irbrc .irbrc
-# ln -sh dotfiles/siegerc .siegerc
-# ln -sh dotfiles/tmux .tmux
-# ln -sh .tmux/tmux.conf .tmux.conf
-# ln -sh dotfiles/vim .vim
-# ln -sh .vim/vimrc .vimrc
-ln -sh projects/dotfiles/zshrc .zshrc
-ln -sh projects/dotfiles/zshrc.env .zshrc.env
-# ln -sh dotfiles/emacs.d .emacs.d
+DOTFILES=~/projects/dotfiles
+
+ln -sh $DOTFILES/zshrc .zshrc
+ln -sh $DOTFILES/plugins/ted/ted.plugin.zsh $ZSH_CUSTOM/plugins/ted/ted.plugin.zsh 
+ln -sh $DOTFILES/vim .vim
+ln -sh $DOTFILES/vimrc .vimrc
---
+++ plugins/ted/ted.plugin.zsh
@@ -0,0 +1,20 @@
+# Aliases
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+
+# FileSearch
+function f() { find . -iname "*$1*" ${@:2} }
+function r() { grep "$1" ${@:2} -R . }
+
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    fg
+    zle redisplay
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z
+
+# test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
