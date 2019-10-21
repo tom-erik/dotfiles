319ea9a201ea7b60eb3f9c562f76e174ed853f09
Tom-Erik Duestad <tom-erik@safeconsult.no>
2019-10-21 07:49:32 +0200

    Fix: vim path

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ ln -sh $DOTFILES/zshrc .zshrc
 if [[ -z $ZSH_CUSTOM/plugins/ted ]]; then
    ln -sh $DOTFILES/plugins/ted $ZSH_CUSTOM/plugins/ted
 fi
-ln -sh $DOTFILES/vim .vim
+ln -sh $DOTFILES/vim/ .vim
 ln -sh $DOTFILES/vimrc .vimrc
