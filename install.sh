1d23de61bd79c625f9483213348e5dfe0ff419ef
Tom-Erik Duestad <tom-erik@safeconsult.no>
2020-06-25 13:25:36 +0200

    Add installs

--- install.sh
+++ install.sh
@@ -10,3 +10,11 @@ ln -sh $DOTFILES/bash_aliases $HOME/.bash_aliases
 ln -sh $DOTFILES/bash_functions $HOME/.bash_functions
 ln -sh $DOTFILES/vimrc $HOME/.vimrc
 ln -s $HOME/Documents/bin $HOME/bin
+
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+brew install fzf
+brew install rg
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
