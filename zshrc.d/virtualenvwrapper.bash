ebe3fc87736d6c4676fa3ee3f50050da7572a74e
Tom-Erik Duestad <tom-erik@safeconsult.no>
2021-03-12 12:22:09 +0100

    Remove files that have been included in zshrc

--- zshrc.d/virtualenvwrapper.bash
+++
@@ -1,9 +0,0 @@
-alias python=/usr/local/bin/python3
-alias pip=/usr/local/bin/pip3
-
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Developer
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
-
-source /usr/local/bin/virtualenvwrapper.sh
